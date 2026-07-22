419e3877e5a61559b90ef1931490237f6c440e08
Lucas Ericsson <lucasericsson222@gmail.com>
2026-07-22 16:11:56 -0700

    Add spikes

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
+
+@onready var hitbox = $Hitbox
+
+func _ready() -> void:
+	hitbox.body_entered.connect(body_entered)
+
+func body_entered(body):
+	queue_free()
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
+	
+	
