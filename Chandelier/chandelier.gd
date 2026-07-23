3401a64bda881b2a24b588aff94bc55abdc36085
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2026-07-23 11:29:56 -0700

    Chandeliers are more stable

--- Chandelier/chandelier.gd
+++ Chandelier/chandelier.gd
@@ -1,35 +1,43 @@
-extends RigidBody2D
+extends Node2D
 
 @onready var ray_cast_2d = $RayCast2D
 @onready var rope_segment = preload("res://Chandelier/RopeSegment/RopeSegment.tscn")
 @onready var rope_end = preload("res://Chandelier/RopeSegment/RopeEnd.tscn")
+@onready var chandelier = $ChandelierRigidBody
+@onready var line_2d = $Line2D
+const SEGMENT_LENGTH = 8
 
+func connect_pin(pos: Vector2, node_a: NodePath, node_b: NodePath) -> void:
+	var joint: PinJoint2D = PinJoint2D.new()
+	joint.position = pos
+	joint.node_a = node_a
+	joint.node_b = node_b
+	add_child(joint)
 
-func _physics_process(_delta: float) -> void:
-	if ray_cast_2d:
-		if ray_cast_2d.is_colliding():
-			var col_point = ray_cast_2d.get_collision_point()
-			var cur_pos_offset = Vector2(0, 0)
-			var target_position = col_point - ray_cast_2d.global_position
+func _physics_process(delta: float) -> void:
+	if ray_cast_2d and ray_cast_2d.is_colliding():
+		var collider = ray_cast_2d.get_collider()
+		var col_point = ray_cast_2d.get_collision_point()
+		var cur_pos_offset = Vector2(0, 0)
+		var target_position = col_point - ray_cast_2d.global_position
+
+		#line_2d.add_point(target_position + ray_cast_2d.position)
+		var segments: Array[Node2D] = []
+		while cur_pos_offset.y > target_position.y:
+			var segment = rope_segment.instantiate()
+			segments.append(segment)
+			segment.position = ray_cast_2d.position + cur_pos_offset
+			add_child(segment)
+			cur_pos_offset.y -= SEGMENT_LENGTH
 			
-			var segments = []
-			while cur_pos_offset.y > target_position.y:
-				var segment = rope_segment.instantiate()
-				add_child(segment)
-				segments.append(segment)
-				
-				segment.position = ray_cast_2d.position + cur_pos_offset
-				cur_pos_offset.y -= 3
-			
-			for i in range(len(segments)):
-				var joint: PinJoint2D = segments[i].get_child(2)
-				if i == 0:
-					joint.node_a = self.get_path()
-				else:
-					joint.node_a = segments[i - 1].get_path()
-				var end = rope_end.instantiate()
-				add_child(end)
-				end.position = ray_cast_2d.position + cur_pos_offset
-				end.get_child(1).node_a = segments[i - 1].get_path()
-			
-			ray_cast_2d.queue_free()
+		var end = rope_end.instantiate()
+		end.position = ray_cast_2d.position + cur_pos_offset
+		segments.append(end)
+		add_child(end)
+		
+		for i in range(len(segments)):
+			if i == 0:
+				connect_pin(segments[i].position, chandelier.get_path(), segments[i].get_path())
+			else:
+				connect_pin(segments[i].position, segments[i - 1].get_path(), segments[i].get_path())
+		ray_cast_2d.queue_free()
