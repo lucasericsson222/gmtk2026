b09ca67a365966ee7f93d0139e9a7069e86e41ad
Lucas Ericsson <lucasericsson222@gmail.com>
2026-07-23 20:56:06 -0700

    add roof, add paralax towers

--- Flame/flame.gd
+++ Flame/flame.gd
@@ -1,6 +1,11 @@
 extends AnimatedSprite2D
 
+@onready var col_shape = $Area2D/CollisionShape2D
 
 func _ready() -> void:
 	speed_scale = randf_range(0.7, 1.3)
+	flip_h = randi_range(0,1)==0
 	play("default")
+
+func _process(_delta) -> void:
+	col_shape.disabled = true if frame < 6 else false
