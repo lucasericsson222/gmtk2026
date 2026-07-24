f64917f7b9d26ac4a9b93d9f6d6371032eb6e58e
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2026-07-24 14:30:17 -0700

    New player animation system

--- Player/climbing.gd
+++ Player/climbing.gd
@@ -3,7 +3,8 @@ class_name ClimbingState
 
 @export var player: CharacterBody2D
 @export var climb_area: Area2D
-@export var animated_sprite: AnimatedSprite2D
+@export var animation_player: AnimationPlayer
+@export var sprite: Sprite2D
 @export var collision_shape: CollisionShape2D
 @export var walking_dust: GPUParticles2D
 
@@ -11,30 +12,30 @@ var left_wall: bool
 const CLIMB_SPEED: float = 100.0
 func _enter() -> void:
 	player.velocity = Vector2.ZERO
-	animated_sprite.play("walking")
-	left_wall = animated_sprite.flip_h
+	animation_player.play("walk")
+	left_wall = sprite.flip_h
 	player.rotate(PI / 2 if left_wall else -PI/2)
 	collision_shape.position.y += 2
 
 func _process(_delta) -> void:
 	var dir = Input.get_axis("up", "down")
 	player.velocity.y = CLIMB_SPEED * dir
-	animated_sprite.play("walking")
+	animation_player.play("walk")
 	if dir == 0.0:
 		walking_dust.emitting = false
-		animated_sprite.play("stand")
+		animation_player.play("stand")
 	else:
 		walking_dust.emitting = true
 	if left_wall:
 		if Input.is_action_pressed("up"):
-			animated_sprite.flip_h = true
+			sprite.flip_h = true
 		if Input.is_action_pressed("down"):
-			animated_sprite.flip_h = false
+			sprite.flip_h = false
 	else:
 		if Input.is_action_pressed("up"):
-			animated_sprite.flip_h = false
+			sprite.flip_h = false
 		if Input.is_action_pressed("down"):
-			animated_sprite.flip_h = true
+			sprite.flip_h = true
 
 	if Input.is_action_just_released("climb"):
 		transition.emit(WalkingState)
