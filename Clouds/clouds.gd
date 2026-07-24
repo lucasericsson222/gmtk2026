03c66ea71c2f9d7e009e7682eee658ee077015b6
Lucas Ericsson <lucasericsson222@gmail.com>
2026-07-24 13:19:18 -0700

    Add background clouds, make it so fire doesn't cover the screen in blue, fire removes tiles

--- Clouds/clouds.gd
+++ Clouds/clouds.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var max_height = 3000
 var num_clouds = 20
+var num_background_clouds = 400
 @onready var cloud_scene = preload("res://Clouds/Cloud.tscn")
 @onready var screen_size = get_viewport_rect().size
-
+@export var cloud_paralax: Parallax2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(num_clouds):
@@ -13,3 +14,9 @@ func _ready() -> void:
 		cloud_instance.position.x = randf_range(0, screen_size.x)
 		cloud_instance.velocity = Vector2(randf_range(0.4,1), 0) * (randi_range(0,1) * 2 - 1)
 		add_child(cloud_instance)
+	for i in range(num_background_clouds):
+		var cloud_instance: AnimatedSprite2D = cloud_scene.instantiate()
+		cloud_instance.position.y = randf_range(0, max_height)
+		cloud_instance.position.x = randf_range(0, screen_size.x)
+		cloud_instance.velocity = Vector2(randf_range(0.1,0.3), 0) * (randi_range(0,1) * 2 - 1)
+		cloud_paralax.add_child(cloud_instance)
--- Flame/flame.gd
+++ Flame/flame.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
 @onready var col_shape = $Area2D/CollisionShape2D
+var tile_layers: Array[TileMapLayer]
 
 func _ready() -> void:
 	speed_scale = randf_range(0.7, 1.3)
@@ -9,3 +10,8 @@ func _ready() -> void:
 
 func _process(_delta) -> void:
 	col_shape.disabled = true if frame < 6 else false
+	
+	if frame > 5:
+		for tile_layer in tile_layers:
+			var tile_at_cloud_level = tile_layer.local_to_map(Vector2(global_position.x, global_position.y))
+			tile_layer.set_cell(tile_at_cloud_level, 0)
